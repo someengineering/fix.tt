db17f6ad0fa786f92f65db769363bc3e07496d2f
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2023-09-04 17:56:48 -0700

    fix: tweak colors, update copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,28 +16,44 @@ import { siteConfig } from '@/constant/config';
 // ];
 
 const features: {
-  name: string;
-  description: string;
+  name: string | JSX.Element;
+  description: JSX.Element;
   icon: (
     props: React.JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>,
   ) => JSX.Element;
 }[] = [
   {
-    name: 'Complete cloud risk visibility',
-    description:
-      'Fix builds an up-to-date representation of your cloud infrastructure on a regular schedule. View all resources and their configuration data in this baseline inventory, for quicker insights and informed decision-making.',
+    name: <abbr title="Cloud Security and Posture Management">CSPM</abbr>,
+    description: (
+      <>
+        Monitor <abbr title="Center for Internet Security">CIS</abbr>{' '}
+        benchmarks, run compliance scans, and enforce policies. Build security
+        intelligence on top of your inventory to stay in control and escalate
+        critical risks.
+      </>
+    ),
     icon: (props) => <LuListChecks {...props} />,
   },
   {
-    name: 'Address the risks that matter',
-    description:
-      'Fix asseses vulnerabilities and misconfigurations based on attack path and business impact. Build security intelligence on top of your inventory to stay in control and escalate the risks that are critical.',
+    name: 'Inventory',
+    description: (
+      <>
+        Get a complete picture of your cloud and see what is running with visual
+        maps. Fix discovers assets and collects rich configuration data for
+        resources, no matter where, when, or how they were provisioned.
+      </>
+    ),
     icon: (props) => <LuBug {...props} />,
   },
   {
-    name: 'Easy remediation',
-    description:
-      'Fix integrates with your workflow, ticketing, and messaging tools to share essential information where it is most useful. Make it easy for your engineers to keep infrastructure updated and tidy.',
+    name: 'Remediation',
+    description: (
+      <>
+        Fix integrates with your workflow, ticketing, and messaging tools. Set
+        up alerts and automations for policy violations to make it easy for your
+        engineers to keep infrastructure updated and tidy.
+      </>
+    ),
     icon: (props) => <LuShuffle {...props} />,
   },
 ];
@@ -48,7 +64,7 @@ const pricingTiers: {
   price: string;
   priceUnit?: string;
   description: string;
-  features: string[];
+  features: (string | JSX.Element)[];
 }[] = [
   {
     name: 'Cloud',
@@ -58,7 +74,10 @@ const pricingTiers: {
     description:
       'For security teams who want a hosted solution to take control of cloud misconfigurations.',
     features: [
-      'CSPM & KSPM',
+      <>
+        <abbr title="Cloud Security and Posture Management">CSPM</abbr> &amp;{' '}
+        <abbr title="Kubernetes Security Posture Management">KSPM</abbr>
+      </>,
       'Compliance checks',
       'Jira integrations',
       'PagerDuty integrations',
@@ -89,9 +108,6 @@ export default function HomePage() {
       <div className="relative isolate px-6 lg:px-8">
         <div className="mx-auto max-w-3xl py-16 sm:py-24 lg:py-28">
           <div className="text-center">
-            <div className="balanced mb-6 rounded-full text-2xl font-semibold leading-6 text-gray-600">
-              {siteConfig.subtitle}
-            </div>
             <h1 className="balanced text-4xl font-bold tracking-tight text-primary-900 sm:text-6xl">
               {siteConfig.tagline}
             </h1>
@@ -106,23 +122,40 @@ export default function HomePage() {
           </div>
         </div>
       </div>
-      <div className="bg-tangerine-50 py-24 sm:py-32">
+      <div className="bg-primary-50 py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
-            <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-              Keep your cloud infrastructure secure without slowing down
-              engineering.
+            <h2 className="text-xl font-semibold leading-7 text-primary-900">
+              Why Fix?
             </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Create a developer experience that pays dividends for security.
+            <p className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+              Detect, prioritize, and remediate critical cloud risks.
             </p>
+            <div className="text-lg leading-8 text-gray-600">
+              <p className="mt-6">
+                <strong>
+                  Fix connects to cloud APIs to take full snapshots of your
+                  cloud infrastructure and technology stack.
+                </strong>{' '}
+                You get a baseline inventory with a complete view of accounts,
+                instances, functions, Kubernetes pods, databases, storage
+                buckets, and all other resources and their configurations in a
+                single place.
+              </p>
+              <p className="mt-6">
+                The Fix Security Graph also shows relationships between
+                resources, providing the context to understand critical risks
+                and attack paths&mdash;so you can prioritize and fix the
+                misconfigurations and vulnerabilities that matter.
+              </p>
+            </div>
           </div>
           <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
             <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
-              {features.map((feature) => (
-                <div key={feature.name} className="flex flex-col">
+              {features.map((feature, index) => (
+                <div key={`feature-${index}`} className="flex flex-col">
                   <dt className="text-lg font-semibold leading-7 text-gray-900">
-                    <div className="mb-6 flex h-10 w-10 items-center justify-center rounded-xl bg-primary-900">
+                    <div className="mb-6 flex h-10 w-10 items-center justify-center rounded-xl bg-jade-600">
                       <feature.icon
                         className="h-6 w-6 text-white"
                         aria-hidden="true"
@@ -198,13 +231,16 @@ export default function HomePage() {
                     role="list"
                     className="mt-10 space-y-4 text-sm leading-6 text-gray-600"
                   >
-                    {tier.features.map((feature) => (
-                      <li key={feature} className="flex gap-x-3">
+                    {tier.features.map((feature, index) => (
+                      <li
+                        key={`pricing-${tier}-${index}`}
+                        className="flex gap-x-3"
+                      >
                         <LuCheck
                           className="h-6 w-6 flex-none text-jade-600"
                           aria-hidden="true"
                         />
-                        {feature}
+                        <span>{feature}</span>
                       </li>
                     ))}
                   </ul>
--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,8 +1,7 @@
 export const siteConfig = {
   title: 'Fix by Some Engineering Inc.',
-  subtitle: 'An open-source Wiz alternative.',
-  tagline: 'Cloud security that fixes vulnerabilities.',
+  tagline: 'Simple and affordable visibility into your cloud security posture.',
   description:
-    'Most security tools generate a list of problems but don’t help cut through the noise. Fix enables security engineers to prioritize and fix the vulnerabilities that matter to their business.',
+    'Fix is an open-source Wiz alternative for cloud infrastructure security. Take control of cloud risks with an asset inventory, compliance scans, and remediation workflows.',
   url: 'https://fix.tt',
 };
