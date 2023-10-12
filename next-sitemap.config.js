d11397dc79b9d4bbfd082e64f2ae70da681f2abb
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2023-10-12 15:09:26 -0700

    feat: workaround for next-sitemap bug

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,11 +5,27 @@
 module.exports = {
   siteUrl: 'https://fix.tt',
   generateRobotsTxt: true,
+  additionalPaths: async (config) =>
+    await Promise.all(
+      ['/', '/code-of-conduct', '/cookie-policy'].map(
+        async (path) => await config.transform(config, path),
+      ),
+    ),
   robotsTxtOptions: {
     policies: [
       process.env.VERCEL_ENV === 'production'
         ? { userAgent: '*', allow: '/' }
         : { userAgent: '*', disallow: '/' },
+      {
+        userAgent: '*',
+        disallow: [
+          '/*.json$',
+          '/*_buildManifest.js$',
+          '/*_middlewareManifest.js$',
+          '/*_ssgManifest.js$',
+          '/*.js$',
+        ],
+      },
     ],
   },
 };
