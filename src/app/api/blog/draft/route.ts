812be849110a9186b2fae316516d566f95e1ddc7
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2023-10-25 14:31:49 -0700

    test: debug

--- src/app/api/blog/draft/route.ts
+++ src/app/api/blog/draft/route.ts
@@ -1,14 +1,50 @@
+import request from 'graphql-request';
 import { type NextRequest, NextResponse } from 'next/server';
 
-import { getHashnodeDraft } from '@/api/hashnode';
+import { HASHNODE_ENDPOINT, HASHNODE_HOST } from '@/constants/hashnode';
+import { HashnodeDraftResponse } from '@/interfaces/hashnode';
+
+export const revalidate = 0;
 
 export async function GET(req: NextRequest) {
   try {
-    return NextResponse.json(
-      await getHashnodeDraft(req.nextUrl.searchParams.get('id') ?? ''),
-      { status: 200 },
+    const variables = {
+      host: HASHNODE_HOST,
+      id: req.nextUrl.searchParams.get('id') ?? '',
+    };
+
+    const query = `
+      query Draft($id: ObjectId!) {
+        draft(id: $id) {
+          id
+          title
+          tags {
+            name
+            slug
+          }
+          author {
+            name
+            profilePicture
+            socialMediaLinks {
+              linkedin
+            }
+          }
+          content {
+            markdown
+          }
+          dateUpdated
+        }
+      }
+    `;
+
+    const data = await request<HashnodeDraftResponse>(
+      HASHNODE_ENDPOINT,
+      query,
+      variables,
     );
+
+    return NextResponse.json(data.draft, { status: 200 });
   } catch (e) {
-    return NextResponse.json({ error: 'Draft not found' }, { status: 404 });
+    return NextResponse.json({ error: JSON.stringify(e) }, { status: 404 });
   }
 }
