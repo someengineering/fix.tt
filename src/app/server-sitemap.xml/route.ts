0a8fbb944d70397c51c55d5e7dc3762a030d2d59
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2023-10-27 11:42:31 -0700

    fix: server sitemap revalidate

--- src/app/server-sitemap.xml/route.ts
+++ src/app/server-sitemap.xml/route.ts
@@ -2,6 +2,9 @@ import { getServerSideSitemap, ISitemapField } from 'next-sitemap';
 
 import { getHashnodePosts, getHashnodeTagSlugs } from '@/api/hashnode';
 import { siteConfig } from '@/constants/config';
+import { isLocal } from '@/constants/env';
+
+export const revalidate = isLocal ? 0 : 300;
 
 export async function GET() {
   const blogPosts = (await getHashnodePosts({}))
