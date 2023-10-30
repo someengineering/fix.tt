0a8e6de685d18dc572d3e4d3efe3137a30ee76d1
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2023-10-30 16:02:04 -0700

    feat: about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,98 @@
+import { Metadata } from 'next';
+
+import MeetOurLeadership from '@/components/MeetOurLeadership';
+
+import { metadata as rootMetadata } from '@/app/layout';
+import { siteConfig } from '@/constants/config';
+import { openGraph } from '@/utils/og';
+
+const url = `${siteConfig.url}/cookie-policy`;
+const title = 'About';
+const description =
+  'We designed Fix to simplify the process of fixing misconfigurations and make it easier for engineering teams to align security and speed.';
+const ogImage = openGraph({
+  title,
+  description,
+});
+
+export const metadata: Metadata = {
+  title,
+  description,
+  alternates: {
+    ...rootMetadata.alternates,
+    canonical: url,
+  },
+  openGraph: {
+    ...rootMetadata.openGraph,
+    url,
+    title,
+    description,
+    images: [ogImage],
+  },
+  twitter: {
+    ...rootMetadata.twitter,
+    title: `${title} | ${siteConfig.title}`,
+    description,
+    images: [ogImage],
+  },
+};
+
+export default function AboutPage() {
+  return (
+    <>
+      <div className="px-6 py-32 lg:px-8">
+        <div className="mx-auto max-w-3xl text-lg leading-7 text-gray-700">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            About Fix
+          </h1>
+          <p className="mt-8 text-xl leading-8">
+            At Fix, our journey began with a simple idea&mdash;to make cloud
+            security accessible and frustration-free.
+          </p>
+          <p className="mt-8 text-xl leading-8">
+            Before we started building Fix, we interviewed and listened to the
+            voices of over forty cloud security engineers.{' '}
+            <strong>
+              One theme resonated above all: the desire to &ldquo;fix all the
+              things.&rdquo;
+            </strong>
+          </p>
+          <p className="mt-8 text-xl leading-8">
+            We all know that you can&rsquo;t fix everything or always ensure
+            100% configuration, but this was their way of expressing frustration
+            with existing processes. The traditional approach often paints
+            security as the bad guy: security surfaces problems and engineering
+            is stuck fixing them. The feedback loop is too long&mdash;security
+            has to talk to engineering, but engineering doesn&rsquo;t have
+            context.
+          </p>
+          <p className="mt-8 text-xl leading-8">
+            We created Fix to bridge this gap and transform security from a
+            blocker to an enabler.
+          </p>
+          <div className="max-w-2xl">
+            <h2 className="mt-12 text-2xl font-bold tracking-tight text-gray-900">
+              Why &ldquo;Fix&rdquo;?
+            </h2>
+            <p className="mt-4">
+              We chose the name &ldquo;Fix&rdquo; because it encapsulates our
+              mission. We designed Fix to simplify the process of fixing
+              misconfigurations and make it easier for engineering teams to
+              align security and speed.
+            </p>
+            <h2 className="mt-12 text-2xl font-bold tracking-tight text-gray-900">
+              Turning frustration into innovation
+            </h2>
+            <p className="mt-4">
+              With Fix, we want to create an operating model where engineering
+              and security teams work hand in hand to achieve their goals. Our
+              goal is to give cloud security engineers the tools to protect
+              their infrastructure without compromising innovation and speed.
+            </p>
+          </div>
+        </div>
+      </div>
+      <MeetOurLeadership />
+    </>
+  );
+}
---
+++ src/components/MeetOurLeadership.tsx
@@ -0,0 +1,185 @@
+import Image from 'next/image';
+import { FaGithub, FaLinkedin } from 'react-icons/fa6';
+
+import PrimaryLink from '@/components/common/links/PrimaryLink';
+
+import larsPhoto from '@/assets/photos/lars.jpg';
+import lukasPhoto from '@/assets/photos/lukas.jpg';
+import matthiasPhoto from '@/assets/photos/matthias.jpg';
+
+const people = [
+  {
+    name: 'Lukas Lösche',
+    role: 'Co-founder & CISO',
+    photo: lukasPhoto,
+    bio: (
+      <>
+        <p className="my-4">
+          My background is in building secure distributed systems&mdash;from
+          bare metal racked up in a data center running Linux, Windows, and
+          FreeBSD to cloud platforms that securely connect vehicles, ships, and
+          airplanes owned by the United States Government.
+        </p>
+        <p className="my-4">
+          At Fix, I spend my time on Zoom calls with our users and turn their
+          needs and feedback into new product capabilities. I built our original
+          open source asset inventory and security graph that&rsquo;s now the
+          foundation for Fix. The cloud is a graph, not a table&mdash;and
+          security tooling should reflect that. I&rsquo;m also our acting{' '}
+          <abbr title="chief information security officer">CISO</abbr>, keeping
+          our own infrastructure and systems secure.
+        </p>
+        <p className="my-4">
+          I&rsquo;m a gamer by heart, and attend Gamescom in Cologne every year.
+          My Tuesday nights are blocked for playing D&amp;D, and I also maintain
+          a pretty popular{' '}
+          <PrimaryLink href="https://github.com/lloesche/valheim-server-docker">
+            Valheim server Docker image
+          </PrimaryLink>
+          .
+        </p>
+      </>
+    ),
+    linkedinUrl: 'https://linkedin.com/in/lloesche',
+    githubUrl: 'https://github.com/lloesche',
+  },
+  {
+    name: 'Lars Kamp',
+    role: 'Co-founder & CEO',
+    photo: larsPhoto,
+    bio: (
+      <>
+        <p className="my-4">
+          I started with economics and statistics, but then found networks and
+          computers much more interesting. My first encounter with cloud
+          infrastructure was when I had to create a financial model for a global
+          network of data centers. Today, that global network is better known as
+          Microsoft Azure.
+        </p>
+        <p className="my-4">
+          At Fix, I&rsquo;m usually the first line of support for customers. I
+          ask a lot of questions about your existing security stack and the
+          compliance frameworks you need to track. Since I come from the cloud
+          warehouse space and analytics engineering, I look at security data as
+          just another data source that needs to be centralized and visualized
+          in a dashboard.
+        </p>
+        <p className="my-4">
+          I love writing about startups with my{' '}
+          <PrimaryLink href="https://buildingdistribution.substack.com">
+            &ldquo;Building Distribution&rdquo; Substack
+          </PrimaryLink>
+          . And I track cloud security companies and open source projects in my{' '}
+          <PrimaryLink href="https://github.com/someengineering/cloud-security-list">
+            &ldquo;Cloud Security List&rdquo;
+          </PrimaryLink>
+          .
+        </p>
+      </>
+    ),
+    linkedinUrl: 'https://linkedin.com/in/larskamp',
+    githubUrl: 'https://github.com/scapecast',
+  },
+  {
+    name: 'Matthias Veit',
+    role: 'Co-founder & CTO',
+    photo: matthiasPhoto,
+    bio: (
+      <>
+        <p className="my-4">
+          Before Fix, I was an Apache committer and the technical lead for
+          Marathon, a highly available cluster scheduler for container
+          orchestration for Apache Mesos. I also built the trip execution and
+          vehicle dispatching system for MOIA, Volkswagen&rsquo;s ride pooling
+          service.
+        </p>
+        <p className="my-4">
+          At Fix, I lead a team of engineers in creating the building blocks of
+          our service. This includes creating a search syntax that enables
+          intuitive searching and traversing of our security graph without the
+          hassle of a learning a complex graph query language. I also ensure
+          that our unified data model works across all resources for{' '}
+          <abbr title="Amazon Web Services">AWS</abbr>, Google Cloud, Azure, and
+          other platforms Fix supports.
+        </p>
+        <p className="my-4">
+          My current passion is reducing the noise in a security
+          engineers&rsquo; life.
+        </p>
+      </>
+    ),
+    linkedinUrl: 'https://linkedin.com/in/matthias-veit',
+    githubUrl: 'https://github.com/aquamatthias',
+  },
+];
+
+export default function MeetOurLeadership() {
+  return (
+    <div className="bg-primary-50 py-24 md:py-32">
+      <div className="mx-auto grid max-w-7xl grid-cols-1 gap-x-12 gap-y-20 px-6 lg:px-8 xl:grid-cols-6">
+        <div className="max-w-2xl xl:col-span-2">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Meet our leadership
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            We&rsquo;ve built products for security, infrastructure,
+            observability, and analytics.
+          </p>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            We think modern cloud security combines the best of data collection,
+            anomaly detection, and presentation of insights in a way that is
+            actionable and understandable by a human.
+          </p>
+        </div>
+        <ul
+          role="list"
+          className="-mt-12 space-y-12 divide-y divide-gray-200 xl:col-span-4"
+        >
+          {people.map((person) => (
+            <li
+              key={person.name}
+              className="flex flex-col items-start gap-x-6 gap-y-10 pt-12 sm:flex-row"
+            >
+              <Image
+                className="aspect-[4/5] w-52 flex-none rounded-2xl object-cover sm:aspect-[2/3]"
+                src={person.photo}
+                alt=""
+              />
+              <div className="max-w-xl flex-auto">
+                <h3 className="text-lg font-semibold leading-8 tracking-tight text-gray-900">
+                  {person.name}
+                </h3>
+                <p className="text-base leading-7 text-gray-600">
+                  {person.role}
+                </p>
+                <div className="mt-6 text-base leading-7 text-gray-600">
+                  {person.bio}
+                </div>
+                <ul role="list" className="mt-6 flex gap-x-4">
+                  <li>
+                    <a
+                      href={person.linkedinUrl}
+                      className="text-gray-400 hover:text-gray-500"
+                    >
+                      <span className="sr-only">LinkedIn</span>
+                      <FaLinkedin className="h-5 w-5" aria-hidden="true" />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href={person.githubUrl}
+                      className="text-gray-400 hover:text-gray-500"
+                    >
+                      <span className="sr-only">GitHub</span>
+                      <FaGithub className="h-5 w-5" aria-hidden="true" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
