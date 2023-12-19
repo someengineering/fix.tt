3a7d3ed227b16976a60e5767ae0413cc84cc670c
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2023-12-19 02:17:29 -0800

    fix: center-align how fix works section

--- src/components/HowFixWorks.tsx
+++ src/components/HowFixWorks.tsx
@@ -3,25 +3,27 @@ import ArchitectureDiagram from '@/assets/diagrams/architecture.svg';
 export default function HowFixWorks() {
   return (
     <section className="py-16 sm:py-24">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
+      <div className="mx-auto max-w-7xl px-6 text-center lg:px-8">
+        <div className="mx-auto max-w-4xl">
           <h2
             className="mb-3 text-lg font-semibold uppercase leading-7 text-marian-blue-800 sm:text-xl"
-            id="why"
+            id="how"
           >
             How Fix works
           </h2>
-          <p className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          <p className="mx-auto mt-2 max-w-prose text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
-          <div className="text-lg leading-8 text-gray-600">
-            <p className="mt-6">
+        </div>
+        <div className="relative mx-auto max-w-prose text-lg leading-8 text-gray-600">
+          <p className="mt-6">
+            <strong>
               Fix collects raw metadata from cloud APIs and transforms it into
-              structured security data. The Fix Security Graph shows
-              relationships between resources, providing the context to
-              understand critical risks and attack paths.
-            </p>
-          </div>
+              structured security data.
+            </strong>{' '}
+            The Fix Security Graph shows relationships between resources,
+            providing the context to understand critical risks and attack paths.
+          </p>
         </div>
         <ArchitectureDiagram className="mx-auto mt-8 max-w-2xl sm:mt-12 lg:max-w-none" />
       </div>
