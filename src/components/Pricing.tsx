96e83aac1a6d5fb52e11f8c9e0fabe246669a0c9
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2024-02-13 03:05:24 -0800

    feat: updated pricing

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,64 +1,111 @@
 import GithubSlugger from 'github-slugger';
-import { LuCheck } from 'react-icons/lu';
+import {
+  LuArmchair,
+  LuBuilding,
+  LuBuilding2,
+  LuCheck,
+  LuPencilRuler,
+  LuPersonStanding,
+  LuWarehouse,
+} from 'react-icons/lu';
 
 import ButtonLink from '@/components/common/links/ButtonLink';
-import PrimaryLink from '@/components/common/links/PrimaryLink';
+import UnstyledLink from '@/components/common/links/UnstyledLink';
+
+import { cn } from '@/utils/css';
 
 const tiers: {
   name: string;
   href: string;
+  icon: (
+    props: React.JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>,
+  ) => JSX.Element;
   cta: string;
   price: string | { monthly: string; annually?: string };
   description: string;
-  targetCustomer: string;
+  maxCloudAccounts: number;
   scanFrequency: string;
+  seats: { included?: number; maximum?: number };
   features: string[];
+  mostPopular?: boolean;
 }[] = [
   {
     name: 'Free',
     href: 'https://app.global.fixcloud.io/auth/register',
+    icon: (props) => <LuPersonStanding {...props} />,
     cta: 'Get started',
     price: '$0',
-    description: 'Single-account security overview on a monthly basis.',
-    targetCustomer:
-      'Perfect for individual use or small-scale proof-of-concept trials.',
+    description:
+      'For solo software engineers who want to secure a single cloud account.',
+    maxCloudAccounts: 1,
     scanFrequency: 'Monthly',
+    seats: { maximum: 1 },
     features: [
-      'Basic asset inventory',
-      'Compliance scans',
-      'Account risk score',
-      'Fix recommendations',
-      'Monthly email report',
-    ],
-  },
-  {
-    name: 'Foundational',
-    href: 'https://app.global.fixcloud.io/auth/register',
-    cta: 'Get started',
-    price: { monthly: '$5' },
-    description: 'Daily scans for secure, compliant operations.',
-    targetCustomer:
-      'Ideal for growing businesses that need a robust security baseline.',
-    scanFrequency: 'Daily',
-    features: [
-      'Alerting integrations (Slack, PagerDuty, Discord)',
-      'Graph visualization',
+      '1-month history',
+      'Asset inventory',
       'Inventory search',
-      'CSV data export',
+      'Neighborhood view',
+      'Security benchmarks',
+      'Monthly email report',
+      'Remediation recommendations',
+      'Core CSPM scanning capabilities',
     ],
   },
   {
-    name: 'High Security',
+    name: 'Plus',
     href: 'https://app.global.fixcloud.io/auth/register',
+    icon: (props) => <LuWarehouse {...props} />,
+    cta: 'Get started',
+    price: { monthly: '$30' },
+    description:
+      'For growing teams looking to stay secure as they build out infrastructure.',
+    maxCloudAccounts: 3,
+    scanFrequency: 'Daily',
+    seats: { included: 2, maximum: 20 },
+    features: [
+      '3-month history',
+      'Email alerts',
+      'Weekly email report',
+      'Data export (CSV, JSON, PDF)',
+    ],
+  },
+  {
+    name: 'Business',
+    href: 'https://app.global.fixcloud.io/auth/register',
+    icon: (props) => <LuBuilding {...props} />,
+    cta: 'Get started',
+    price: { monthly: '$40' },
+    description:
+      'For engineering teams looking to automate their cloud infrastructure security.',
+    maxCloudAccounts: 10,
+    scanFrequency: 'Hourly',
+    seats: { included: 5, maximum: 50 },
+    features: [
+      '6-month history',
+      'Custom policies (coming soon!)',
+      'Alerting integrations (PD, Slack, Discord, Teams)',
+      'Task management integrations (coming soon!)',
+    ],
+    mostPopular: true,
+  },
+  {
+    name: 'Enterprise',
+    href: 'https://app.global.fixcloud.io/auth/register',
+    icon: (props) => <LuBuilding2 {...props} />,
     cta: 'Get started',
     price: { monthly: '$50' },
-    description: 'Hourly scans for critical, fast-paced environments.',
-    targetCustomer:
-      'Advanced integration for top-tier security needs and IaC support.',
+    description:
+      'For dedicated security teams looking to built an integrated security toolchain.',
+    maxCloudAccounts: 25,
     scanFrequency: 'Hourly',
+    seats: { included: 20 },
     features: [
-      'Alerting integrations with custom HTTP webhooks',
-      'Automatic inventory exports (AWS S3)',
+      '18-month history',
+      'API access',
+      'Custom alerting webhooks',
+      'Single Sign on (coming soon!)',
+      'Workspace analytics (coming soon!)',
+      'Snowflake data export (coming soon!)	',
     ],
   },
 ];
@@ -77,68 +124,75 @@ export default function Pricing() {
             Pricing
           </h2>
           <p className="mx-auto mt-2 max-w-prose text-balance text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-            Start for free, upgrade as you grow.
+            Foundational AWS security for your whole company. Free for engineers
+            to try.
           </p>
         </div>
         <div className="relative mx-auto max-w-prose text-balance text-lg leading-8 text-gray-600">
           <p className="mt-6">
-            Fix pricing is based on the number of cloud accounts you scan. There
-            is no minimum commitment, and you can even start scanning a single
-            cloud account for free.
-          </p>
-          <p className="mt-6">
-            Use your existing budget to pay for Fix, with convenient billing
-            through AWS Marketplace.
+            Fix pricing scales on a per-cloud-account basis, with an add-on to
+            buy more seats for your team. We offer a free tier and two-week
+            trials.
           </p>
         </div>
-        <div className="my-20 flow-root">
-          <div className="isolate mx-auto -mt-16 grid max-w-sm grid-cols-1 items-stretch gap-y-16 divide-y divide-gray-100 text-left lg:-mx-8 lg:mt-0 lg:max-w-none lg:grid-cols-3 lg:divide-x lg:divide-y-0 xl:-mx-4">
+        <div className="mt-20 flow-root">
+          <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 items-stretch gap-8 text-left md:max-w-2xl md:grid-cols-2 lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-4">
             {tiers.map((tier, index) => (
               <div
                 key={`tier-${slugger.slug(tier.name)}`}
-                className="flex flex-col pt-16 lg:px-8 lg:pt-0 xl:px-14 "
+                className={cn(
+                  tier.mostPopular
+                    ? 'ring-2 ring-marian-blue-600'
+                    : 'ring-1 ring-gray-200',
+                  'flex flex-col rounded-3xl p-8',
+                )}
               >
                 <h3
                   id={`tier-${slugger.slug(tier.name)}`}
-                  className="text-2xl font-semibold leading-7 text-marian-blue-900"
+                  className="flex items-center gap-3 text-3xl font-semibold leading-7 text-marian-blue-900"
                 >
+                  <tier.icon />
                   {tier.name}
+                  {tier.mostPopular ? (
+                    <span className="inline-flex items-center rounded-full bg-marian-blue-100 px-2 py-1 text-xs font-medium text-marian-blue-600 xl:hidden">
+                      Most popular
+                    </span>
+                  ) : null}
                 </h3>
                 <p className="mt-6 text-base leading-6 text-gray-900">
-                  <span className="font-bold">{tier.description}</span>{' '}
-                  {tier.targetCustomer}
+                  {tier.description}
                 </p>
-                <p className="my-9 flex items-baseline gap-x-1 border-b border-gray-900/10 pb-9">
+                <p className="my-8 flex items-baseline gap-x-1 border-b border-gray-900/10 pb-8 xl:flex-col">
                   {typeof tier.price === 'string' ? (
                     <>
-                      <span className="text-5xl font-bold tracking-tight text-gray-900">
+                      <span className="text-4xl font-bold tracking-tight text-gray-900 xl:mb-6">
                         {tier.price}
-                      </span>
-                      <span className="ml-1.5 text-sm font-semibold leading-6 text-gray-600">
-                        one cloud account included*
                       </span>
                     </>
                   ) : (
                     <>
-                      <span className="text-5xl font-bold tracking-tight text-gray-900">
+                      <span className="text-4xl font-bold tracking-tight text-gray-900">
                         {tier.price.monthly}
                       </span>
-                      <span className="text-sm font-semibold leading-6 text-gray-600">
-                        / cloud account per month*
+                      <span className="ml-1 text-sm font-semibold leading-6 text-gray-600 xl:ml-0">
+                        per cloud account, per month
                       </span>
                     </>
                   )}
                 </p>
-                <p className="text-base font-semibold leading-6 text-gray-900">
-                  Scan frequency
-                </p>
-                <p className="mt-1.5 text-base leading-6 text-gray-600">
-                  {tier.scanFrequency}
-                </p>
+                <div className="gap-y-2 text-base leading-6 text-gray-600">
+                  <p>{tier.maxCloudAccounts} cloud account max</p>
+                  <p>{tier.scanFrequency} scans</p>
+                  <p>
+                    {tier.seats.included
+                      ? `${tier.seats.included} seats included${tier.seats.maximum ? ` (${tier.seats.maximum} max)` : ''}`
+                      : `${tier.seats.maximum} seat${tier.seats.maximum === 1 ? '' : 's'} max`}
+                  </p>
+                </div>
                 <p className="mt-6 text-base font-semibold leading-6 text-gray-900">
                   {index === 0
-                    ? `${tier.name} features:`
-                    : `All ${tiers[index - 1].name} features, plus:`}
+                    ? 'Features:'
+                    : `Everything in ${tiers[index - 1].name}, and:`}
                 </p>
                 <ul
                   role="list"
@@ -154,46 +208,52 @@ export default function Pricing() {
                     </li>
                   ))}
                 </ul>
-                <ButtonLink href={tier.href} className="mt-9 block text-center">
+                <ButtonLink
+                  href={tier.href}
+                  variant={tier.mostPopular ? 'default' : 'outline'}
+                  className="mt-9 block text-center"
+                >
                   {tier.cta}
                 </ButtonLink>
               </div>
             ))}
           </div>
         </div>
-        <p className="mx-auto my-20 max-w-prose space-y-2 text-base leading-7 text-gray-600">
-          * Our fair-use policy allows for up to 200,000 resources per account.
-          If your needs surpass this amount, please{' '}
-          <PrimaryLink href="mailto:hi@fix.security">reach out</PrimaryLink> to
-          discuss your specific requirements.
-        </p>
-        <div className="flex flex-col items-start gap-x-8 gap-y-6 rounded-3xl p-8 ring-1 ring-gray-900/10 sm:gap-y-10 sm:p-10 lg:col-span-2 lg:flex-row lg:items-center">
-          <div className="text-left lg:min-w-0 lg:flex-1">
-            <h3 className="text-2xl font-semibold leading-8 tracking-tight text-marian-blue-900">
-              Enterprise (Custom)
-            </h3>
-            <p className="mt-6 max-w-prose text-base leading-7 text-gray-600">
-              <span className="font-semibold">
-                Custom deployments in your own infrastructure (on-premises or in
-                the cloud).
-              </span>{' '}
-              Best for government agencies and companies in regulated
-              industries.
-            </p>
-            <p className="mt-6 max-w-prose text-base leading-7 text-gray-600">
-              With the Enterprise tier, you can tailor Fix exactly to your
-              requirements. You will work with a dedicated solution architect
-              and security engineer to integrate Fix into your toolchain,
-              workflows, and security protocols.
-            </p>
+        <div className="mb-20 mt-5 flow-root">
+          <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 items-stretch gap-8 text-left md:max-w-2xl lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-2">
+            <div className="flex flex-col rounded-3xl p-8 ring-1 ring-gray-200">
+              <h3 className="flex items-center gap-3 text-3xl font-semibold leading-7 text-marian-blue-900">
+                <LuArmchair />
+                Additional seats
+              </h3>
+              <p className="my-8 flex items-baseline gap-x-1">
+                <span className="text-4xl font-bold tracking-tight text-gray-900">
+                  $5
+                </span>
+                <span className="ml-1 text-sm font-semibold leading-6 text-gray-600">
+                  per seat, per month
+                </span>
+              </p>
+              <p className="text-base leading-6 text-gray-600">
+                Add additional seats to any paid plan.
+              </p>
+            </div>
+            <div className="flex flex-col rounded-3xl p-8 ring-1 ring-gray-200">
+              <h3 className="flex items-center gap-3 text-3xl font-semibold leading-7 text-marian-blue-900">
+                <LuPencilRuler />
+                Custom plans
+              </h3>
+              <p className="my-8 text-xl font-semibold leading-8 text-gray-600">
+                <UnstyledLink href="mailto:hi@fix.security">
+                  Schedule a call with us &rarr;
+                </UnstyledLink>
+              </p>
+              <p className="text-base leading-6 text-gray-600">
+                Need more seats, support for custom data sources, or private
+                deployment options?
+              </p>
+            </div>
           </div>
-          <ButtonLink
-            href="mailto:hi@fix.security"
-            variant="outline"
-            className="inline"
-          >
-            Talk to an expert <span aria-hidden="true">&rarr;</span>
-          </ButtonLink>
         </div>
       </div>
     </section>
