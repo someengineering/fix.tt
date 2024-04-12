017e3680f9fec51b6f2674609b38eeb466c63df2
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2024-04-12 11:45:27 -0700

    fix: default env values

--- src/constants/posthog.ts
+++ src/constants/posthog.ts
@@ -1,4 +1,6 @@
 export const POSTHOG_PROJECT_API_KEY =
   process.env.NEXT_PUBLIC_POSTHOG_PROJECT_API_KEY;
-export const POSTHOG_API_HOST = process.env.NEXT_PUBLIC_POSTHOG_API_HOST;
-export const POSTHOG_UI_HOST = process.env.NEXT_PUBLIC_POSTHOG_UI_HOST;
+export const POSTHOG_API_HOST =
+  process.env.NEXT_PUBLIC_POSTHOG_API_HOST || '/api/ingest';
+export const POSTHOG_UI_HOST =
+  process.env.NEXT_PUBLIC_POSTHOG_UI_HOST || 'https://eu.posthog.com';
