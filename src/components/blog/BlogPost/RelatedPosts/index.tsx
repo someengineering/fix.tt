5003d906f48249b59bb8f4d0d2018caeb907ee35
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2024-04-26 13:15:18 -0700

    fix(blog): add anchors for related posts & newsletter form

--- src/components/blog/BlogPost/RelatedPosts/index.tsx
+++ src/components/blog/BlogPost/RelatedPosts/index.tsx
@@ -27,7 +27,10 @@ export default async function RelatedPosts({
   }
 
   return (
-    <section className="mx-auto mt-16 max-w-7xl px-6 pt-16 sm:mt-24 sm:pt-24 lg:px-8">
+    <section
+      className="mx-auto mt-16 max-w-7xl px-6 pt-16 sm:mt-24 sm:pt-24 lg:px-8"
+      id="related-posts"
+    >
       <h2 className="text-3xl font-extrabold sm:text-4xl">Related posts</h2>
       <div className="mx-auto mt-8 grid max-w-md grid-cols-1 gap-x-8 gap-y-16 md:mx-0 md:max-w-none md:grid-cols-2 lg:grid-cols-3">
         {posts.edges
