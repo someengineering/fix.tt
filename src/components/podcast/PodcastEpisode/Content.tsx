b3a970aa5e063bc7e0a31c8117ff53fa34d33a66
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2024-05-13 22:33:52 -0700

    fix(podcast): render PrimaryLink for description links

--- src/components/podcast/PodcastEpisode/Content.tsx
+++ src/components/podcast/PodcastEpisode/Content.tsx
@@ -1,11 +1,27 @@
-import parse from 'html-react-parser';
+import parse, { DOMNode, Element, domToReact } from 'html-react-parser';
 
 import { sanitizeHtml } from '@/utils/transistor';
+import PrimaryLink from '@/components/common/links/PrimaryLink';
+
+const parserOptions = {
+  replace: (domNode: DOMNode) => {
+    if (domNode instanceof Element) {
+      switch (domNode.name) {
+        case 'a':
+          return (
+            <PrimaryLink href={domNode.attribs.href}>
+              {domToReact(domNode.children as DOMNode[], parserOptions)}
+            </PrimaryLink>
+          );
+      }
+    }
+  },
+};
 
 export default function Content({ html }: { html: string }) {
   return (
     <div className="w-prose html my-8 border-y border-gray-900/5 pt-8">
-      {parse(sanitizeHtml(html))}
+      {parse(sanitizeHtml(html), parserOptions)}
     </div>
   );
 }
