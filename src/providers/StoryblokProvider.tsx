2045484dd856288bceca0cf36891a6d1bf3fc698
Igor Nazarov <ingerbird@gmail.com>
2024-08-14 16:36:16 +0400

    Using public token on client provider

--- src/providers/StoryblokProvider.tsx
+++ src/providers/StoryblokProvider.tsx
@@ -5,7 +5,7 @@ import { apiPlugin, storyblokInit } from '@storyblok/react';
 import components from '../../storyblok';
 
 storyblokInit({
-  accessToken: process.env.STORYBLOK_OAUTH_TOKEN,
+  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_OAUTH_TOKEN,
   use: [apiPlugin],
   components,
 });
